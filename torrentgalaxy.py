ced7cb1c7de00f4fd7c2b04d686f8a94543d1bb1
frindle27 <sudarshannikhil27@gmail.com>
2024-10-25 00:40:10 +0530

    Add publish date handling and make minor improvements

--- torrentgalaxy.py
+++ torrentgalaxy.py
@@ -1,7 +1,8 @@
 from bs4 import BeautifulSoup
+from concurrent.futures import ThreadPoolExecutor
+from datetime import datetime
 from novaprinter import prettyPrinter
 import requests
-import threading
 
 class torrentgalaxy(object):
     url = 'https://torrentgalaxy.to/'
@@ -9,15 +10,13 @@ class torrentgalaxy(object):
     supported_categories = {
         'all': '',
         'anime': 'c28=1',
-        'apps': 'c20=1&c21=1&c18=1',
         'books': 'c13=1&c19=1&c12=1&c14=1&c15=1',
-        'docus': 'c9=1',
         'games': 'c43=1&c10=1',
         'movies': 'c3=1&c46=1&c45=1&c42=1&c4=1&c1=1',
         'music': 'c22=1&c26=1&c23=1&c25=1&c24=1',
-        'other': 'c17=1&c40=1&c37=1&c33=1',
+        'pictures': 'c37=1',
+        'software': 'c20=1&c21=1&c18=1',
         'tv': 'c41=1&c5=1&c11=1&c6=1&c7=1',
-        'xxx': 'c48=1&c35=1&c47=1&c34=1',
     }
 
     def retrieve_url(self, url):
@@ -26,7 +25,7 @@ class torrentgalaxy(object):
             response.raise_for_status() # Raises HTTPError for bad HTTP response (e.g., 404 or 500)
             return response.text # Return the HTML content of the page
         
-        except requests.exceptions.HTTPError as e:
+        except requests.exceptions.RequestException as e:
             print(f'Error fetching URL {url}: {e}')
             return None
 
@@ -37,7 +36,9 @@ class torrentgalaxy(object):
 
         # Each row is a torrent
         for row in soup.find_all('div', class_='tgxtablerow'):
-            torrent = {}
+            torrent = {
+                'engine_url': self.url
+            }
 
             # Name and link
             title_column = row.find('a', class_='txlight')
@@ -63,6 +64,25 @@ class torrentgalaxy(object):
             if download_link:
                 torrent['link'] = download_link['href']
 
+            # Date and time published
+            columns = row.find_all('div', class_='tgxtablecell collapsehide rounded txlight')
+            if columns:
+                date_column = columns[-1]
+                date = date_column.find('small')
+
+                if date:
+                    date_str = date.text.strip()
+
+                    try:
+                        # Parse date string to a datetime object
+                        date_obj = datetime.strptime(date_str, '%d/%m/%y %H:%M')
+                        
+                        # Convert datetime object to UNIX timestamp (in seconds)
+                        torrent['pub_date'] = int(date_obj.timestamp())
+
+                    except ValueError:
+                        print(f'Error parsing date: {date_str}')
+
             if torrent:
                 torrents.append(torrent)
 
@@ -80,6 +100,15 @@ class torrentgalaxy(object):
             
         return 1
     
+    def fetch_and_print_page(self, url):
+        html_content = self.retrieve_url(url)
+
+        if html_content:
+            torrents = self.parse_html(html_content)
+
+            for torrent in torrents:
+                prettyPrinter(torrent)
+    
     # DO NOT CHANGE the name and parameters of this function
     # This function will be the one called by nova2.py
     def search(self, what, cat='all'):
@@ -95,32 +124,14 @@ class torrentgalaxy(object):
         # Parse and print results from the first page
         torrents = self.parse_html(html_content)
         for torrent in torrents:
-            torrent['engine_url'] = self.url # Add engine URL
-            torrent['pub_date'] = -1
             prettyPrinter(torrent)
 
         # Handle pagination (fetch additional pages using multithreading)
         total_pages = self.get_number_of_pages(html_content)
 
         if total_pages > 1:
-            threads = []
-
-            for page in range(1, total_pages): # Pages are 0 indexed in torrentgalaxy.to
-                page_url = f'{search_url}&page={page}'
-                thread = threading.Thread(target=self.fetch_and_print_page, args=(page_url,))
-                threads.append(thread)
-                thread.start()
-            
-            # Wait for all threads to complete
-            for thread in threads:
-                thread.join()
-
-    def fetch_and_print_page(self, url):
-        html_content = self.retrieve_url(url)
-
-        if html_content:
-            torrents = self.parse_html(html_content)
-
-            for torrent in torrents:
-                torrent['engine_url'] = self.url # Add engine URL
-                prettyPrinter(torrent)
+            with ThreadPoolExecutor(max_workers=5) as executor:
+                # Generate and execute tasks for remaining pages
+                for page in range(1, total_pages):
+                    page_url = f'{search_url}&page={page}'
+                    executor.submit(self.fetch_and_print_page, page_url)
